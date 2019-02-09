f247559a73dbd9aec10a310c96020b5529569ab4
jpancho <jpancho@sfu.ca>
2019-02-08 19:35:18 -0800

    Implement json object to send using express

--- src/Server.js
+++ src/Server.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3001;
+
+// This is our json object to send to localhost:3001
+let performers = [
+  {
+    name: 'Justin',
+    attribute: 'Singer',
+    price: '20.0',
+    location: 'Vancouver'
+  },
+  {
+    name: 'Sarah',
+    attribute: 'Singer',
+    price: '40.0',
+    location: 'Coquitlam'
+  }
+];
+
+app.use(cors());
+
+// using express, we can send our json object
+app.get('/', function (req, res) {
+  res.send(performers)
+});
 
 app.listen(PORT, function() {
   console.log("Server running on Port: " + PORT);
